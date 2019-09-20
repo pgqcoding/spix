ec98ab0b103146b692259a43ce36f25df260a4c2
Ethan.pang <156468884@qq.com>
2019-09-20 16:39:53 +0800

    project init
    Signed-off-by: Ethan.pang <156468884@qq.com>

---
+++ src/main/java/com/pgqcoding/spix/SpixApplication.java
@@ -0,0 +1,13 @@
+package com.pgqcoding.spix;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpixApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpixApplication.class, args);
+    }
+
+}
